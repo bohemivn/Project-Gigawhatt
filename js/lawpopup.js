8761119ea924d10b4d09e7bf711a4923846deb55
JB Keyclacker <85263183+bohemivn@users.noreply.github.com>
2024-12-07 21:15:21 -0500

    Add files via upload

---
+++ js/lawpopup.js
@@ -0,0 +1,3 @@
+
+
+alert("Laws change! We try to keep this website as up-to-date as possible, but we may be lagged behind in order to write well about the changes. We recommend always consulting multiple resources when it comes to current law.");// JavaScript Document
